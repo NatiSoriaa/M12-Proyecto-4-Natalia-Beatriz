c28bab6e4d56a6c83c86818f0195188e4f894fc3
beatrizabadj <beatrizabadjim@gmail.com>
2025-08-28 18:25:35 +0200

    manage options cors

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,4 +1,15 @@
 <?php
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === "https://earth-project-frontend.vercel.app/") {
+    header("Access-Control-Allow-Origin: https://earth-project-frontend.vercel.app/");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit(0);
+}
 
 header("Access-Control-Allow-Origin: https://earth-project-frontend.vercel.app/");
 header("Access-Control-Allow-Credentials: true");
